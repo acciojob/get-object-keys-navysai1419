15f766cc0079b4a0c69199b2885de9685d840a59
Navya <88023981+navysai1419@users.noreply.github.com>
2024-02-12 21:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+//your JS code here. If req
+student={name:navya,
+		 _proto_{
+			 getKeys(){
+				 for(key in student)
+					return key
+			 }
+		 }
+		
+		
+		}
+student.geTkeys();
+
+
+
